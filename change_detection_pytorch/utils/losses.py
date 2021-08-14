6e4c251d96c6fa27e4609ccf9e78a802a6415e04
likyoo <1150249769@qq.com>
2021-08-14 21:09:18 +0800

    rm sth. in losses.py

--- change_detection_pytorch/utils/losses.py
+++ change_detection_pytorch/utils/losses.py
@@ -4,43 +4,44 @@ from . import base
 from . import functional as F
 from ..base.modules import Activation
 
+# See change_detection_pytorch/losses
 
-class JaccardLoss(base.Loss):
-
-    def __init__(self, eps=1., activation=None, ignore_channels=None, **kwargs):
-        super().__init__(**kwargs)
-        self.eps = eps
-        self.activation = Activation(activation)
-        self.ignore_channels = ignore_channels
-
-    def forward(self, y_pr, y_gt):
-        y_pr = self.activation(y_pr)
-        return 1 - F.jaccard(
-            y_pr, y_gt,
-            eps=self.eps,
-            threshold=None,
-            ignore_channels=self.ignore_channels,
-        )
-
-
-class DiceLoss(base.Loss):
-
-    def __init__(self, eps=1., beta=1., activation=None, ignore_channels=None, **kwargs):
-        super().__init__(**kwargs)
-        self.eps = eps
-        self.beta = beta
-        self.activation = Activation(activation)
-        self.ignore_channels = ignore_channels
-
-    def forward(self, y_pr, y_gt):
-        y_pr = self.activation(y_pr)
-        return 1 - F.f_score(
-            y_pr, y_gt,
-            beta=self.beta,
-            eps=self.eps,
-            threshold=None,
-            ignore_channels=self.ignore_channels,
-        )
+# class JaccardLoss(base.Loss):
+#
+#     def __init__(self, eps=1., activation=None, ignore_channels=None, **kwargs):
+#         super().__init__(**kwargs)
+#         self.eps = eps
+#         self.activation = Activation(activation)
+#         self.ignore_channels = ignore_channels
+#
+#     def forward(self, y_pr, y_gt):
+#         y_pr = self.activation(y_pr)
+#         return 1 - F.jaccard(
+#             y_pr, y_gt,
+#             eps=self.eps,
+#             threshold=None,
+#             ignore_channels=self.ignore_channels,
+#         )
+#
+#
+# class DiceLoss(base.Loss):
+#
+#     def __init__(self, eps=1., beta=1., activation=None, ignore_channels=None, **kwargs):
+#         super().__init__(**kwargs)
+#         self.eps = eps
+#         self.beta = beta
+#         self.activation = Activation(activation)
+#         self.ignore_channels = ignore_channels
+#
+#     def forward(self, y_pr, y_gt):
+#         y_pr = self.activation(y_pr)
+#         return 1 - F.f_score(
+#             y_pr, y_gt,
+#             beta=self.beta,
+#             eps=self.eps,
+#             threshold=None,
+#             ignore_channels=self.ignore_channels,
+#         )
 
 
 class L1Loss(nn.L1Loss, base.Loss):
