76eb8f0a9bb06b2e66cd62c1300b9d25dad5828c
likyoo <1150249769@qq.com>
2021-08-16 09:15:41 +0800

    fix UNet++ out_channels

--- change_detection_pytorch/unetplusplus/decoder.py
+++ change_detection_pytorch/unetplusplus/decoder.py
@@ -73,6 +73,7 @@ class UnetPlusPlusDecoder(Decoder):
             attention_type=None,
             center=False,
             fusion_form="concat",
+            seg_ensemble=None,
     ):
         super().__init__()
 
@@ -110,19 +111,17 @@ class UnetPlusPlusDecoder(Decoder):
         blocks = {}
         for layer_idx in range(len(self.in_channels) - 1):
             for depth_idx in range(layer_idx+1):
-                if depth_idx == 0:
-                    in_ch = self.in_channels[layer_idx]
-                    skip_ch = self.skip_channels[layer_idx] * (layer_idx+1)
-                    out_ch = self.out_channels[layer_idx]
-                else:
-                    out_ch = self.skip_channels[layer_idx]
-                    skip_ch = self.skip_channels[layer_idx] * (layer_idx+1-depth_idx)
-                    in_ch = self.skip_channels[layer_idx - 1]
+                out_ch = self.out_channels[layer_idx]
+                skip_ch = self.out_channels[layer_idx] * (layer_idx-depth_idx) + self.skip_channels[layer_idx]
+                in_ch = self.skip_channels[layer_idx - 1] if depth_idx == layer_idx and depth_idx != 0 else self.in_channels[layer_idx]
+
                 blocks[f'x_{depth_idx}_{layer_idx}'] = DecoderBlock(in_ch, skip_ch, out_ch, **kwargs)
         blocks[f'x_{0}_{len(self.in_channels)-1}'] =\
             DecoderBlock(self.in_channels[-1], 0, self.out_channels[-1], **kwargs)
         self.blocks = nn.ModuleDict(blocks)
         self.depth = len(self.in_channels) - 1
+        self.seg_ensemble = seg_ensemble
+        self.ECAM = md.ECAM(in_channels=self.out_channels[-2], out_channels=self.out_channels[-1]) if seg_ensemble == "ecam" else None
 
     def forward(self, *features):
 
@@ -132,7 +131,7 @@ class UnetPlusPlusDecoder(Decoder):
         features = features[::-1]  # reverse channels to start from head of encoder
         # start building dense connections
         dense_x = {}
-        for layer_idx in range(len(self.in_channels)-1):
+        for layer_idx in range(len(self.in_channels) - 1):
             for depth_idx in range(self.depth-layer_idx):
                 if layer_idx == 0:
                     output = self.blocks[f'x_{depth_idx}_{depth_idx}'](features[depth_idx], features[depth_idx+1])
@@ -143,5 +142,9 @@ class UnetPlusPlusDecoder(Decoder):
                     cat_features = torch.cat(cat_features + [features[dense_l_i+1]], dim=1)
                     dense_x[f'x_{depth_idx}_{dense_l_i}'] =\
                         self.blocks[f'x_{depth_idx}_{dense_l_i}'](dense_x[f'x_{depth_idx}_{dense_l_i-1}'], cat_features)
-        dense_x[f'x_{0}_{self.depth}'] = self.blocks[f'x_{0}_{self.depth}'](dense_x[f'x_{0}_{self.depth-1}'])
-        return dense_x[f'x_{0}_{self.depth}']
+
+        if self.seg_ensemble == "ecam":
+            return self.ECAM([dense_x[f'x_{i}_{self.depth - 1}'] for i in range(self.depth)])
+        else:
+            dense_x[f'x_{0}_{self.depth}'] = self.blocks[f'x_{0}_{self.depth}'](dense_x[f'x_{0}_{self.depth - 1}'])
+            return dense_x[f'x_{0}_{self.depth}']
