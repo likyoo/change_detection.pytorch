9348d4c6d1e9df82f9dd687438f533df9ac5cf6a
likyoo <1150249769@qq.com>
2021-08-18 20:12:45 +0800

    add 'map_location' when loading models

--- change_detection_pytorch/encoders/__init__.py
+++ change_detection_pytorch/encoders/__init__.py
@@ -1,5 +1,6 @@
 import functools
 
+import torch
 import torch.utils.model_zoo as model_zoo
 
 from ._preprocessing import preprocess_input
@@ -21,6 +22,8 @@ from .timm_sknet import timm_sknet_encoders
 from .timm_universal import TimmUniversalEncoder
 from .vgg import vgg_encoders
 from .xception import xception_encoders
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 encoders = {}
 encoders.update(resnet_encoders)
@@ -72,7 +75,7 @@ def get_encoder(name, in_channels=3, depth=5, weights=None, output_stride=32, **
             raise KeyError("Wrong pretrained weights `{}` for encoder `{}`. Available options are: {}".format(
                 weights, name, list(encoders[name]["pretrained_settings"].keys()),
             ))
-        encoder.load_state_dict(model_zoo.load_url(settings["url"]))
+        encoder.load_state_dict(model_zoo.load_url(settings["url"], map_location=torch.device(DEVICE)))
 
     encoder.set_in_channels(in_channels, pretrained=weights is not None)
     if output_stride != 32:
