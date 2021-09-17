2f470d98d4fdb404e88c6590dedae0ada04f40d7
likyoo <1150249769@qq.com>
2021-09-17 10:09:56 +0800

    add Mosaic

--- change_detection_pytorch/datasets/transforms/albu.py
+++ change_detection_pytorch/datasets/transforms/albu.py
@@ -149,3 +149,118 @@ class ExchangeTime(BasicTransform):
             kwargs['image'], kwargs['image_2'] = kwargs['image_2'], kwargs['image']
 
         return kwargs
+
+
+class RandomChoice(BasicTransform):
+    """Apply single transformation randomly picked from a list.
+    """
+
+    def __init__(self, transforms, always_apply=True, p=1.0):
+        super(RandomChoice, self).__init__(always_apply=always_apply, p=p)
+        if not isinstance(transforms, Sequence):
+            raise TypeError("Argument transforms should be a sequence")
+        self.transforms = transforms
+
+    def __call__(self, force_apply=False, **kwargs):
+        t = random.choice(self.transforms)
+        return t(force_apply=force_apply, **kwargs)
+
+
+class Mosaic(BasicTransform):
+    """ Mosaic?
+    Args:
+        size (int): input_size / 2
+        img_infos (dict): a dict with img info.
+        dataset_size (int): The size (len) of dataset.
+        p (float): probability of applying the transform. Default: 0.5
+    Targets:
+        image, mask
+    Image types:
+        uint8, float32
+    """
+
+    def __init__(
+            self,
+            size=256,
+            img_infos=None,
+            dataset_size=None,
+            always_apply=False,
+            p=0.5,
+    ):
+        super(Mosaic, self).__init__(always_apply, p)
+        self.size = size
+        self.img_infos = img_infos
+        self.dataset_size = dataset_size
+
+    def __call__(self, force_apply=False, **kwargs):
+        if self.replay_mode:
+            if self.applied_in_replay:
+                return self.apply_with_params(self.params, **kwargs)
+
+            return kwargs
+
+        if (random.random() < self.p) or self.always_apply or force_apply:
+            params = self.get_params()
+            for key, arg in kwargs.items():
+                kwargs[key] = self.apply(arg, key, **params)
+
+        return kwargs
+
+    def get_image(self, img_info, img_key="image", vis=False):
+        """Open and read the image.
+        Args:
+            img_info (dict): a dict with img info.
+            img_key (str):
+            vis (bool): Whether to use visualization (debug mode).
+        Returns:
+            dict: image info with new keys.
+        """
+
+        if img_key == "image":
+            img = cv2.cvtColor(cv2.imread(img_info['img']['img1_path']), cv2.COLOR_BGR2RGB)
+        elif img_key == "image_2":
+            img = cv2.cvtColor(cv2.imread(img_info['img']['img2_path']), cv2.COLOR_BGR2RGB)
+        elif img_key == "mask":
+            img = cv2.imread(img_info['ann']['ann_path'], cv2.IMREAD_GRAYSCALE)
+            img = img / 255 if not vis else img
+        else:
+            raise ValueError("Undefined key: {}".format(img_key))
+        return img
+
+    def apply(self, img, img_key="image", **params):
+
+        img1 = random_crop(img, self.size, self.size, params["h_start_1"], params["w_start_1"])
+
+        img_info_2 = self.img_infos[params["index_2"]]
+        img2 = self.get_image(img_info_2, img_key)
+        img2 = random_crop(img2, self.size, self.size, params["h_start_2"], params["w_start_2"])
+
+        img_info_3 = self.img_infos[params["index_3"]]
+        img3 = self.get_image(img_info_3, img_key)
+        img3 = random_crop(img3, self.size, self.size, params["h_start_3"], params["w_start_3"])
+
+        img_info_4 = self.img_infos[params["index_4"]]
+        img4 = self.get_image(img_info_4, img_key)
+        img4 = random_crop(img4, self.size, self.size, params["h_start_4"], params["w_start_4"])
+
+        img = np.concatenate([np.concatenate([img1, img2], axis=1),
+                              np.concatenate([img3, img4], axis=1)], axis=0)
+
+        return img
+
+    def get_params(self):
+        return {"h_start_1": random.random(), "w_start_1": random.random(),
+                "h_start_2": random.random(), "w_start_2": random.random(),
+                "h_start_3": random.random(), "w_start_3": random.random(),
+                "h_start_4": random.random(), "w_start_4": random.random(),
+                "index_2": random.randint(0, self.dataset_size-1),
+                "index_3": random.randint(0, self.dataset_size-1),
+                "index_4": random.randint(0, self.dataset_size-1),}
+
+    def get_transform_init_args_names(self):
+        return (
+            "size",
+            "img_infos",
+            "dataset_size",
+            )
+
