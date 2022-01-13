57d47f44bf62e5d989513cab1475b02b265f1b29
e1y4r <e1y4r@outlook.com>
2022-01-13 14:32:08 +0800

    Added kappa and dice score to metrics.

--- change_detection_pytorch/utils/metrics.py
+++ change_detection_pytorch/utils/metrics.py
@@ -99,4 +99,40 @@ class Precision(base.Metric):
         )
 
 
+class Dice(base.Metric):
+
+    def __init__(self,  eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.dice(
+            y_pr, y_gt,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class Kappa(base.Metric):
+
+    def __init__(self,  eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.kappa(
+            y_pr, y_gt,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
 
